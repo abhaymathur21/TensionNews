a13edfd476f4c435d9e2a617d83d7764d09248ca
Abhay Mathur <a21.mathur21@gmail.com>
2024-03-09 23:09:03 +0530

    extracting descriptions and keywords from meta tags of articles

--- backend/app.py
+++ backend/app.py
@@ -32,10 +32,35 @@ with open('backend/google_serp_api.json', 'r') as file:
     #     print(entry)
     
 article_links = []
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299"
+  }
 
-# for entry in data:
-#     article_links.append(entry["link"])
+for entry in data:
+    article_links.append(entry["link"])
     
+article_description = []
+article_keywords = []
+article_title = []
+  
+for link in article_links:
+    response_ = requests.get(link, headers=headers)
+    soup_ = BeautifulSoup(response_.content, "lxml")
+    title = soup_.find("title")
+    article_title.append(title.text)
+    desc_meta_tag = soup_.find('meta', attrs={'name': 'description'})
+    if desc_meta_tag:
+        description = desc_meta_tag.get('content')
+        article_description.append(description)
+        # print(description)
+        
+    keywords_meta_tag = soup_.find('meta', attrs={'name': 'keywords'})
+    if keywords_meta_tag:
+        keywords = keywords_meta_tag.get('content')
+        article_keywords.append(keywords)
+        # print(keywords)
+
+
 vector_embeddings = []
 embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
 
@@ -49,23 +74,23 @@ for entry in data:
 
 if __name__ == '__main__':
     
-    with app.app_context():
+    # with app.app_context():
                         
-        db.create_all()    
+    #     db.create_all()    
         
-        for i, entry in enumerate(data, start=0):
-            # Create a new SerpTest object for each entry
+    #     for i, entry in enumerate(data, start=0):
+    #         # Create a new SerpTest object for each entry
             
             
             
-            new_entry = SerpTest(source_json=entry, position_in_search = entry["position"], title = entry["title"], link_to_article = entry["link"], snippet = entry["snippet"], source = entry["source"], vectors = vector_embeddings[i])
+    #         new_entry = SerpTest(source_json=entry, position_in_search = entry["position"], title = entry["title"], link_to_article = entry["link"], snippet = entry["snippet"], source = entry["source"], vectors = vector_embeddings[i])
             
-            # Add the new entry to the session
-            db.session.add(new_entry)
+    #         # Add the new entry to the session
+    #         db.session.add(new_entry)
             
-            db.session.commit()
+    #         db.session.commit()
                 
-        db.session.close()
+    #     db.session.close()
 
 
     app.run()
