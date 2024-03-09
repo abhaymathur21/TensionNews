d5b2cee5c5b3b5dcf4d349da2b380944153a7763
Shreya Shah <shreyashah100803@gmail.com>
2024-03-10 01:21:17 +0530

    tag similarity added

--- similarity.py
+++ similarity.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 import numpy as np
 from sqlalchemy.dialects.postgresql import ARRAY
 from datetime import datetime
+from sqlalchemy.dialects.postgresql import JSONB
 
 app = Flask(__name__)
 
@@ -22,6 +23,7 @@ class SerpTest(db.Model):
     snippet = db.Column(db.String)
     source = db.Column(db.String)
     vectors = db.Column(db.JSON)
+    tags = db.Column(JSONB)
 
 # Function to compute cosine similarity
 def compute_similarity(vector1, vector2):
@@ -33,12 +35,11 @@ def compute_similarity(vector1, vector2):
     return similarity
 
 def calculate_similarity():
-    print("hello")
+    # print("hello")
     # Fetch vectors from the database
     vectors = db.session.query(SerpTest.vectors).all()
     vectors = [np.array(vector[0]) for vector in vectors]
     # print(vectors)
-
     
     num_rows = len(vectors)
     
@@ -53,12 +54,51 @@ def calculate_similarity():
             if isinstance(vector2, dict):
                 vector2 = np.array(vector2['vectors'])  # Convert dictionary to NumPy array
             similarity_matrix[i, j] = compute_similarity(vector1, vector2)
-    print("Similarity_Matrix: ", similarity_matrix.tolist())
+    # print("Similarity_Matrix: ", similarity_matrix.tolist())
 
     return jsonify({"Similarity_Matrix": similarity_matrix.tolist()}), 200
 
+def tag_similarity():
+    print("tag!")
+    # Fetch tags from the database
+    tags = db.session.query(SerpTest.tags).all()
+    num_rows = len(tags)
+    
+    # Initialize similarity matrix with zeros
+    tag_sim = np.zeros((num_rows, num_rows))
+    
+    # Calculate tag similarity
+    for i in range(num_rows):
+        for j in range(num_rows):
+            tag1 = tags[i]
+            tag2 = tags[j]
+            tag_sim[i][j] = calculate_tag_similarity(tag1, tag2)
+            
+    print("Similarity_Matrix: ", tag_sim.tolist())
+    return jsonify({"Similarity_Matrix": tag_sim.tolist()}), 200
+
+def calculate_tag_similarity(tags1, tags2):
+    tags1_list = tags1[0].split(",") if tags1 else []
+    tags2_list = tags2[0].split(",") if tags2 else []
+    
+    print("Tags1:", tags1_list)
+    print("Tags2:", tags2_list)
+    
+    if not tags1_list or not tags2_list:
+        print("One of the tag lists is empty.")
+        return 0  # Return 0 if either of the tag lists is empty
+    
+    common_tags = len(set(tags1_list) & set(tags2_list))  # Count common tags
+    print("Common Tags:", common_tags)
+    
+    similarity = common_tags / len(tags1_list)  # Calculate similarity
+    print("Similarity:", similarity)
+    
+    return similarity
+
+
 if __name__ == '__main__':
     with app.app_context():
-        calculate_similarity()
+        tag_similarity()
     app.run(debug=True)
     
