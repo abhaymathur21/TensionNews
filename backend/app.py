dee02529559fb35607191db6e45e5c64d7e943cc
Abhay Mathur <a21.mathur21@gmail.com>
2024-03-10 09:50:12 +0530

    added stock_price and volume columns

--- backend/app.py
+++ backend/app.py
@@ -9,6 +9,7 @@ from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddi
 
 from llm import llm_response
 from metadata import extract_metadata
+from stock_analysis.stock_analysis import stock_extraction
 
 db = SQLAlchemy()
 app = Flask(__name__)
@@ -20,40 +21,45 @@ class SerpTest(db.Model):
     __tablename__ = 'SerpTest'
     id = db.Column(db.Integer, primary_key=True)
     created_at = db.Column(db.DateTime(timezone=True), default=datetime.utcnow)
-    source_json = db.Column(db.JSON)
     position_in_search = db.Column(db.Integer)
+    date_of_news = db.Column(db.String)
     title = db.Column(db.String)
     link_to_article = db.Column(db.String)
     snippet = db.Column(db.String)
     tags = db.Column(JSONB)
     source = db.Column(db.String)
     vectors = db.Column(db.JSON)
-    
+    company = db.Column(db.String)
+    location = db.Column(db.String)
+    closing_price = db.Column(db.JSON)
+    volume = db.Column(db.JSON)
+        
 db.init_app(app)
 
-with open('backend/google_serp_api.json', 'r') as file:
-    data = json.load(file)
+# with open('backend/google_serp_api.json', 'r') as file:
+#     data = json.load(file)
 
-article_links = []
-vector_embeddings = []
-embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+# article_links = []
+# vector_embeddings = []
+# embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
 
-for entry in data:
-    article_links.append(entry["link"])
+# for entry in data:
+#     article_links.append(entry["link"])
 
-article_description, article_keywords, article_title = extract_metadata(article_links)
+# article_description, article_keywords, article_title = extract_metadata(article_links)
 
-for article in article_description:
-    vector_embeddings.append(embeddings.embed_query(article))
+# for article in article_description:
+#     vector_embeddings.append(embeddings.embed_query(article))
+
     
-# TO ADD NEW ENTRIES TO THE DATABASE, UNCOMMENT THE FOLLOWING CODE:  
+# # TO ADD NEW ENTRIES TO THE DATABASE, UNCOMMENT THE FOLLOWING CODE:  
     
 # with app.app_context():
 #     db.create_all()        
     
 #     for i, entry in enumerate(data, start=0):
                 
-#         new_entry = SerpTest(source_json=entry, position_in_search = entry["position"], title = entry["title"], link_to_article = entry["link"], snippet = article_description[i], tags= article_keywords[i], source = entry["source"], vectors = vector_embeddings[i])
+#         new_entry = SerpTest(position_in_search = entry["position"], date_of_news = entry["date"], title = entry["title"], link_to_article = entry["link"], snippet = article_description[i], tags= article_keywords[i], source = entry["source"], vectors = vector_embeddings[i])
         
 #         # Add the new entry to the session
 #         db.session.add(new_entry)
@@ -64,10 +70,28 @@ for article in article_description:
     
     
 with app.app_context():    
+    
+    db_data = SerpTest.query.all()
+    
+    article_links = []
+    vector_embeddings = []
+    embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+
+    for row in db_data:
+        article_links.append(row.link_to_article)
+
+    article_description, article_keywords, article_title = extract_metadata(article_links)
+
+    for article in article_description:
+        vector_embeddings.append(embeddings.embed_query(article))
+
+    
     company = []
     location = []
+    company_stock_symbol = []
+    closing_price = []
+    volume = []
 
-    db_data = SerpTest.query.all()
 
     for row in db_data:
         title = row.title
@@ -75,14 +99,39 @@ with app.app_context():
         snippet = row.snippet
         tags = row.snippet
         source = row.source
+        date_of_news = row.date_of_news
         
         company_temp, location_temp = llm_response(title, link_to_article, snippet, tags, source)
         company.append(company_temp)
         location.append(location_temp)
         
-    print(company)
-    print("---------")
-    print(location)
+        if company_temp != 'None':
+            closing_price_temp, volume_temp = stock_extraction(company_temp, date_of_news)
+            closing_price.append(closing_price_temp)
+            volume.append(volume_temp)
+        else:
+            closing_price.append(None)
+            volume.append(None)
+        
+    
+    for i, row in enumerate(db_data, start=0):
+                
+        new_entry = SerpTest(position_in_search = row.position_in_search, date_of_news = row.date_of_news, title = row.title, link_to_article = row.link_to_article, snippet = article_description[i], tags= article_keywords[i], source = row.source, vectors = vector_embeddings[i], company = company[i], location = location[i], closing_price = closing_price[i], volume = volume[i])
+        
+        # Add the new entry to the session
+        db.session.add(new_entry)
+        
+        db.session.commit() 
+        
+    db.session.close()
+
+        
+        
+    # print(company)
+    # print("---------")
+    # print(location)
+    
+    
 
 if __name__ == '__main__':
     
